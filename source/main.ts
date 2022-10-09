983a9152b5a34e83d453baa02ac8742e335ae426
SpacingBat3 <git@spacingbat3.anonaddy.com>
2022-10-09 20:49:27 +0200

    Simple protocol implementation.
    • Gather body chunks and parse it to JSON.
    • Implement simple JSON request structure.
    • Add more comments.

--- source/main.ts
+++ source/main.ts
@@ -7,20 +7,55 @@ import type { IncomingMessage, ServerResponse } from "http";
 
 type protocolVersion = 1;
 
+/** Used for message groups in `console.log`. */
 const logger = {
-  server: kolor.purple(kolor.bold("[SERVER]"))
+  server: kolor.purple(kolor.bold("[SERVER]")),
+  handler: kolor.yellow(kolor.bold("[HANDLER]"))
+}
+
+function teapotError(res:ServerResponse) {
+  res.writeHead(418,"I'm a teapot!");
+  res.write(JSON.stringify({status: "Cannot brew ☕️ with a 🫖️!"}));
+  res.end();
 }
 
 function requestHandler(request:IncomingMessage, response: ServerResponse, protocolVersion: protocolVersion) {
+  // Set response MIME type to JSON.
   response.setHeader("Content-Type","application/json");
+  // Handle `/api/v1/send` requests.
   if(request.method === "POST" && request.url === `/api/v${protocolVersion}/send`) {
-    response.writeHead(201,"Vote has been given on nobody!");
-    response.write(JSON.stringify({message:"Hello world!"}));
+    let chunkArray: number[] = [];
+    // Gather chunks to the `chunkArray`.
+    request.on("data", (chunk: Buffer) => {
+      // TODO: Show chunk hash, detect UTF-8 data.
+      console.log("%s Received a chunk of data (0x%s).", logger.handler, chunk.toString("hex"));
+      chunkArray.push(...chunk);
+    });
+    // Handle request once message has been fully sent.
+    request.on("end", () => {
+      try {
+        // Parse chunk array to JSON
+        const parsedChunk:Record<string,string|number|undefined> = JSON.parse(Buffer.from(chunkArray).toString());
+        // Get values from JSON record
+        const {candidate} = parsedChunk;
+        // Verify types
+        if(typeof candidate !== "number")
+          throw new TypeError("Invalid body type!");
+        // Proceed with the voting
+        response.writeHead(201,"Vote has been given!");
+        response.write(JSON.stringify({status: `Vote has been given on candidate '${candidate}'!`}));
+        response.end();
+      } catch {
+        // TODO: Error for invalid messages
+        teapotError(response);
+      }
+    });
+    // Teapot error!
+    request.on("error", () => teapotError(response));
   } else {
-    response.writeHead(418,"I'm a teapot!");
-    response.write(JSON.stringify({message:"Cannot brew ☕️ with a 🫖️!"}));
+    // TODO: Error for invalid URIs
+    teapotError(response);
   }
-  response.end();
 }
 
 function createServer(protocol: protocolVersion, port: number) {
@@ -29,11 +64,14 @@ function createServer(protocol: protocolVersion, port: number) {
     throw new TypeError("The specified protocol version is not of the type 'number'.")
   if(typeof port !== "number")
     throw new TypeError("The server port is of invalid type (should be a 'number' in range: 0-65536)");
+  
   // Check parameter ranges
   if(port < 0 && port > 65536)
     throw new RangeError(`Port '${port}' outside of allowed range 0-65536.`);
   if(protocol !== 1)
     throw new RangeError(`Server implementation of protocol version 'v${(protocol as number).toString()}' is currently unsupported!`);
+  
+  // Start server
   console.log("%s Setting up a server of protocol version v%s.", logger.server, protocol);
   const server = createHTTPServer((req,res) => requestHandler(req,res,protocol));
   server.listen(port);
